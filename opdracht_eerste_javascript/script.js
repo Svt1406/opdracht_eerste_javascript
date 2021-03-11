67fb05a8e06ca145153b6d43d97bb0b0cfcaa1fa
Svt1406 <79196867+Svt1406@users.noreply.github.com>
2021-03-11 15:03:52 +0100

    Add files via upload

---
+++ opdracht_eerste_javascript/script.js
@@ -0,0 +1,19 @@
+console.log('Hello winc Academy')
+let firstName = 'Suleika';
+console.log(firstName);
+let numbers = 4 + 4;
+console.log(numbers);
+let strings = "4" + "4";
+console.log(strings);
+let addition = 2+ 450;
+console.log(addition);
+let subtraction= 4509 - 30;
+console.log(subtraction);
+let multiplication = 403 * 4;
+console.log(multiplication);
+let modulus = 30 % 7;
+console.log(modulus);
+let age = 33;
+console.log(typeof age);
+let ageText = "33";
+console.log(typeof ageText);
